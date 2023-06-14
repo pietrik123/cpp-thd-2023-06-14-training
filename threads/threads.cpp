48aeb282aaa9e3284685f24bec0d0db9da16b894
Krystian Piekos <krystian.piekos@infotraining.pl>
2023-06-14 10:50:03 +0200

    Threads - creating

--- threads/threads.cpp
+++ threads/threads.cpp
@@ -34,7 +34,7 @@ public:
     {
     }
 
-    void operator()(std::chrono::milliseconds delay) const
+    void operator()(std::chrono::milliseconds delay = 100ms) const
     {
         std::cout << "BW#" << id_ << " has started..." << std::endl;
 
@@ -53,6 +53,30 @@ int main()
 {
     std::cout << "Main thread starts..." << std::endl;
     const std::string text = "Hello Threads";
+
+    std::thread thd_empty;
+
+    std::cout << "thd_empty.id = " << thd_empty.get_id() << "\n";
+
+    // Option 1 - function
+    std::thread thd_1{&background_work, 1, "THREADING", 200ms};
+    std::thread thd_2{&background_work, 2, "multi", 150ms};
+
+    // Option 2 - functor
+    BackgroundWork bw{3, "BACKGROUN_DWORK"}; // bw - lvalue
+    std::thread thd_3{std::cref(bw)};
+    std::thread thd_4{BackgroundWork(4, "bw4"), 300ms}; // BackgroundWork(4, "bw4") is rvalue -> move semantics
+
+    // Option 3 - lambda expression
+    const int id = 5;
+    const std::string text = "TEXT";
+    std::thread thd_5{[id, &text](){ background_work(id, text, 400ms); }}; 
+
+    thd_1.join();
+    thd_2.join();
+    thd_3.join();
+    thd_4.join();
+    thd_5.join();
 
     std::cout << "Main thread ends..." << std::endl;
 }
