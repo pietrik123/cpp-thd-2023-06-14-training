79c5f55afa69aea36d26d87d168e7a92936d76ef
Krystian Piekos <krystian.piekos@infotraining.pl>
2023-06-15 11:41:20 +0200

    Monte Carlo Pi with mutex

--- _exercises/monte-carlo-pi/pi.cpp
+++ _exercises/monte-carlo-pi/pi.cpp
@@ -9,6 +9,8 @@
 
 using namespace std::literals;
 
+//inline constexpr size_t hardware_destructive_interference_size = 64U;
+
 /*******************************************************
  * https://academo.org/demos/estimating-pi-monte-carlo
  ******************************************************/
@@ -68,6 +70,50 @@ void calc_hits_with_padding(const uintmax_t count, Hits& hits)
         double y = rnd(rnd_gen);
         if (x * x + y * y < 1)
             hits.value++;
+    }
+}
+
+template <typename T, typename TMutex = std::mutex>
+struct SynchronizedValue
+{
+    T value;
+    TMutex mtx_value;
+
+    void lock()
+    {
+        mtx_value.lock();
+    }
+
+    void unlock()
+    {
+        mtx_value.unlock();
+    }
+
+    std::lock_guard<TMutex> with_lock() // since C++17
+    {
+        return std::lock_guard{mtx_value};
+    }
+};
+
+void calc_hits_with_mutex(const uintmax_t count, SynchronizedValue<uintmax_t>& hits)
+{
+    std::mt19937_64 rnd_gen(std::hash<std::thread::id>{}(std::this_thread::get_id()));
+    std::uniform_real_distribution<double> rnd(0, 1.0);
+
+    uintmax_t local_hits{};
+    for (uintmax_t n = 0; n < count; ++n)
+    {
+        double x = rnd(rnd_gen);
+        double y = rnd(rnd_gen);
+        if (x * x + y * y < 1)
+        {            
+            local_hits++;
+        }
+    }
+
+    {
+        auto lck = hits.with_lock();
+        hits.value += local_hits;
     }
 }
 
@@ -173,7 +219,7 @@ int main()
 
         auto elapsed_time = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
 
-        std::cout << "Pi (multi) = " << pi << std::endl;
+        std::cout << "Pi (multi - local counter) = " << pi << std::endl;
         std::cout << "Elapsed = " << elapsed_time << "ms" << std::endl;
     }
 
@@ -212,7 +258,39 @@ int main()
 
         auto elapsed_time = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
 
-        std::cout << "Pi (multi) = " << pi << std::endl;
+        std::cout << "Pi (multi - padding) = " << pi << std::endl;
+        std::cout << "Elapsed = " << elapsed_time << "ms" << std::endl;
+    }
+
+    std::cout << "///////////////////////////////////////////////////" << std::endl;
+
+    {
+        const auto no_of_cores = std::max(1u, std::thread::hardware_concurrency());
+
+        const uintmax_t n_per_thread = N / no_of_cores;
+
+        std::vector<std::thread> threads;
+        SynchronizedValue<uintmax_t> hits{};
+
+        auto start = std::chrono::high_resolution_clock::now();
+
+        for (auto i = 0; i < no_of_cores; i++)
+        {
+            threads.emplace_back(&calc_hits_with_mutex, n_per_thread, std::ref(hits));
+        }
+
+        for (auto& th : threads)
+        {
+            th.join();
+        }
+
+        const double pi = static_cast<double>(hits.value) / N * 4;
+
+        auto end = std::chrono::high_resolution_clock::now();
+
+        auto elapsed_time = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+
+        std::cout << "Pi (mutex) = " << pi << std::endl;
         std::cout << "Elapsed = " << elapsed_time << "ms" << std::endl;
     }
 }
