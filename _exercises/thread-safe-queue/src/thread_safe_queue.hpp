658c7d3d40abb35d210df732d7060b72bfcd35f8
Krystian Piekos <krystian.piekos@infotraining.pl>
2023-06-16 10:44:41 +0200

    ThreadSafeQueue

--- _exercises/thread-safe-queue/src/thread_safe_queue.hpp
+++ _exercises/thread-safe-queue/src/thread_safe_queue.hpp
@@ -8,6 +8,71 @@
 template <typename T>
 class ThreadSafeQueue
 {
+    std::queue<T> q_;
+    mutable std::mutex mtx_q_;
+    std::condition_variable cv_q_not_empty_;
+
+public:
+    bool empty() const
+    {
+        std::lock_guard lk(mtx_q_);
+        return q_.empty();
+    }
+
+    void push(const T& item)
+    {
+        {
+            std::lock_guard lk(mtx_q_);
+            q_.push(item);
+        }
+        cv_q_not_empty_.notify_one();
+    }
+
+    void push(T&& item)
+    {
+        {
+            std::lock_guard lk(mtx_q_);
+            q_.push(std::move(item));
+        }
+        cv_q_not_empty_.notify_one();
+    }
+
+    void push(std::initializer_list<T> lst)
+    {
+        {
+            std::lock_guard lk(mtx_q_);
+            for (const auto& item : lst)
+            {
+                q_.push(item);
+            }
+        }
+
+        if (lst.size() > 1)
+            cv_q_not_empty_.notify_all();
+        else
+            cv_q_not_empty_.notify_one();
+    }
+
+    bool try_pop(T& element)
+    {
+        std::unique_lock lk{mtx_q_, std::try_to_lock};
+        if (!lk.owns_lock() || q_.empty())
+        {
+            return false;
+        }
+        element = q_.front();
+        q_.pop();
+        return true;
+    }
+
+    void pop(T& item)
+    {
+        std::unique_lock lk{mtx_q_};
+        cv_q_not_empty_.wait(lk, [this]() { return !q_.empty(); });
+
+        item = q_.front();
+        q_.pop();
+    }
 };
 
 #endif // THREAD_SAFE_QUEUE_HPP
