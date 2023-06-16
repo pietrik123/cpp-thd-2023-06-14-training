08b083c79570fec1f621bb1d7953213bca99345c
Krystian Piekos <krystian.piekos@infotraining.pl>
2023-06-16 15:23:57 +0200

    packaged_task

--- futures/futures.cpp
+++ futures/futures.cpp
@@ -34,6 +34,20 @@ void save_to_file(const std::string& filename)
     std::cout << "File saved: " << filename << std::endl;
 }
 
+template <typename Task>
+[[nodiscard]] auto spawn_task(Task&& task)
+{
+    using ResultT = decltype(task());
+
+    std::packaged_task<ResultT()> pt{std::forward<Task>(task)};
+    std::future<ResultT> f = pt.get_future();
+    
+    std::thread thd{std::move(pt)};
+    thd.detach();
+    
+    return f;
+}
+
 int main()
 {
     std::future<int> f1 = std::async(std::launch::deferred, calculate_square, 11);
@@ -41,7 +55,7 @@ int main()
     std::future<int> f3 = std::async(std::launch::async, [] { return calculate_square(30); });
     std::future<void> fsave = std::async(std::launch::async, save_to_file, "data.txt");
 
-    //assert(f2.wait_for(1ms) == std::future_status::deferred);
+    // assert(f2.wait_for(1ms) == std::future_status::deferred);
 
     // while (f1.wait_for(50ms) != std::future_status::ready)
     // {
@@ -72,11 +86,10 @@ int main()
     {
         vec_future_squares[0].get(); // throws future_error
     }
-    catch(const std::future_error& e)
+    catch (const std::future_error& e)
     {
         std::cout << "Many calls to get() not allowed...\n";
     }
-    
 
     std::shared_future shared_f_save = fsave.share();
 
@@ -94,4 +107,39 @@ int main()
 
     thd1.join();
     thd2.join();
+
+    std::cout << "\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\" << std::endl;
+
+    {
+        auto fs1 = std::async(std::launch::async, save_to_file, "file1.dat");
+        ///
+        auto fs2 = std::async(std::launch::async, save_to_file, "file2.dat");
+        ///
+        auto fs3 = std::async(std::launch::async, save_to_file, "file3.dat");
+        ///
+        auto fs4 = std::async(std::launch::async, save_to_file, "file4.dat");
+    }
+
+    std::cout << "\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\" << std::endl;
+
+    std::packaged_task<int()> pt1{[] { return calculate_square(13);}};
+
+    std::future<int> f_pt1 = pt1.get_future();
+
+    //pt1(); // synchronous call
+
+    std::thread thd_worker{std::move(pt1)}; // // asynchronous call
+
+    std::cout << "f_pt1: " << f_pt1.get() << std::endl;
+    
+    thd_worker.join();
+
+    {
+        auto fst1 = spawn_task( [] { save_to_file("file1.dat"); });
+        spawn_task( [] { save_to_file("file2.dat"); });
+        spawn_task( [] { save_to_file("file3.dat"); });
+        spawn_task( [] { save_to_file("file4.dat"); });
+
+        fst1.wait(); // waiting for finishing the task
+    }
 }
